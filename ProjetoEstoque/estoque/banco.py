0597f066c4e5a2fda47039cfb85b7033908389bd
leinadaz <nicolasleinad01@gmail.com>
2025-01-28 20:41:33 -0300

    correção banco

--- ProjetoEstoque/estoque/banco.py
+++ ProjetoEstoque/estoque/banco.py
@@ -24,11 +24,25 @@ def carregar_dados():
             descarte = json.load(file)
 
 def salvar_dados():
+    # Salva o estoque sempre
     with open('backup/estoque.json', 'w') as file:
         json.dump(estoque, file, indent=4)
-    with open('backup/entradas.json', 'w') as file:
-        json.dump(entradas, file, indent=4)
-    with open('backup/saidas.json', 'w') as file:
-        json.dump(saidas, file, indent=4)
+
+    # Salva apenas se houver dados em 'entradas'
+    if entradas:
+        with open('backup/entradas.json', 'w') as file:
+            json.dump(entradas, file, indent=4)
+    elif os.path.exists('backup/entradas.json'):
+        os.remove('backup/entradas.json')  # Remove o arquivo se estiver vazio
+
+    # Salva apenas se houver dados em 'saidas'
+    if saidas:
+        with open('backup/saidas.json', 'w') as file:
+            json.dump(saidas, file, indent=4)
+    elif os.path.exists('backup/saidas.json'):
+        os.remove('backup/saidas.json')  # Remove o arquivo se estiver vazio
+
+    # Salva o descarte sempre
     with open('backup/descarte.json', 'w') as file:
         json.dump(descarte, file, indent=4)
+
